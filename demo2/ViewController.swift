4827d8d21bfc21c22b9644c711e142a1d2bc37fc
sneh <snehnakrani123@gmail.com>
2022-04-12 22:05:06 -0700

    i changed in viewDidLoad

--- demo2/ViewController.swift
+++ demo2/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+print("Hello guys chai peloo ! garam hei")
     }
 
 
